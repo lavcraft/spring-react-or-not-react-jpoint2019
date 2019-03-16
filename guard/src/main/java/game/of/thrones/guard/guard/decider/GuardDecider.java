5f78bff3ceadf768db78b924897ecdbb41ad34ed
Tolkachev Kirill <lavcraft@gmail.com>
2019-03-17 01:26:25 +0300

    Use custom task for resolve letter when it does't have resource for handle

--- guard/src/main/java/game/of/thrones/guard/guard/decider/GuardDecider.java
+++ guard/src/main/java/game/of/thrones/guard/guard/decider/GuardDecider.java
@@ -5,11 +5,14 @@ import game.of.thrones.guard.guard.notifier.Notifier;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tools.ant.taskdefs.condition.Not;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -17,23 +20,25 @@ import java.util.concurrent.TimeUnit;
 public class GuardDecider {
     private final Notifier notifier;
     private final Counter  counter;
+    private final ThreadPoolExecutor letterProcessorExecutor;
 
-    public GuardDecider(Notifier notifier, MeterRegistry meterRegistry) {
+    public GuardDecider(Notifier notifier,
+                        MeterRegistry meterRegistry,
+                        ThreadPoolExecutor letterProcessorExecutor) {
         this.notifier = notifier;
         this.counter = meterRegistry.counter("letter.rps");
+        this.letterProcessorExecutor = letterProcessorExecutor;
     }
 
-    @Async("letterProcessorExecutor")
+//    @Async("letterProcessorExecutor")
     public void decide(Notification notification) {
-        log.info("Received from decoder: " + notification);
-        String message = "Author of the letter with id:" + notification.getLetterId() + " is " + getDecision();
-        notification.setMessage(message);
-        notifier.sendNotification(notification);
-        counter.increment();
+        letterProcessorExecutor.execute(
+                new GuardTask(notification, notifier, counter)
+        );
     }
 
     @SneakyThrows
-    private String getDecision() {
+    private static String getDecision() {
         TimeUnit.SECONDS.sleep(1);
         int decision = (int) ((Math.random() * (2)) + 1);
         log.info(String.valueOf(decision));
@@ -41,6 +46,23 @@ public class GuardDecider {
             return "dangerous, We send a squad of guards to you......hold him!!!";
         } else {
             return "not dangerous. But you can kill him just because it is Game of Thrones!";
+        }
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    public static class GuardTask implements Runnable {
+        private final Notification notification;
+        private final Notifier notifier;
+        private final Counter  counter;
+
+        @Override
+        public void run() {
+            log.info("Received from decoder: " + notification);
+            String message = "Author of the letter with id:" + notification.getLetterId() + " is " + getDecision();
+            notification.setMessage(message);
+            notifier.sendNotification(notification);
+            counter.increment();
         }
     }
 }
